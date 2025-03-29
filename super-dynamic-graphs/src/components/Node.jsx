cf5d085b8cfe7d46c98eaf4dfd257923b75a9e71
daniel G <djdaniel1100@gmail.com>
2025-03-28 21:39:20 -0700

    non-obtrusive logos added

--- super-dynamic-graphs/src/components/Node.jsx
+++ super-dynamic-graphs/src/components/Node.jsx
@@ -1,16 +1,30 @@
-const Node = ({ x, y, onMouseDown }) => {
-    return (
+const Node = ({ x, y, color, icon, onMouseDown }) => {
+  const ICON_SIZE = 24;
+  const NODE_RADIUS = 20;
+
+  return (
+    <g transform={`translate(${x},${y})`}>
       <circle
-        cx={x}
-        cy={y}
-        r="20"
-        fill="#ff4444"
+        r={NODE_RADIUS}
+        fill={color || '#ff4444'}
         stroke="#fff"
         strokeWidth="2"
         onMouseDown={onMouseDown}
         style={{ cursor: 'grab' }}
       />
-    );
-  };
-  
-  export default Node;
+      {icon && (
+        <image
+          href={icon}
+          x={-ICON_SIZE/2}
+          y={-ICON_SIZE/2}
+          width={ICON_SIZE}
+          height={ICON_SIZE}
+          preserveAspectRatio="xMidYMid meet"
+          pointerEvents="none" // Add this line
+        />
+      )}
+    </g>
+  );
+};
+
+export default Node;
--- super-dynamic-graphs/src/utils/graphUtils.js
+++ super-dynamic-graphs/src/utils/graphUtils.js
@@ -2,14 +2,16 @@
 
 // Transform API response to internal format
 export const transformGraphData = (apiData) => {
-    const nodes = (apiData.nodes || []).map(node => ({
-      id: node.id,
-      x: node.x || Math.random() * 800 + 100,
-      y: node.y || Math.random() * 600 + 100,
-      vx: 0,
-      vy: 0,
-      connections: [
-        ...new Set([
+  const nodes = (apiData.nodes || []).map(node => ({
+    id: node.id,
+    x: node.x || Math.random() * 800 + 100,
+    y: node.y || Math.random() * 600 + 100,
+    color: node.color, // Add color property
+    icon: node.icon,   // Add icon property
+    vx: 0,
+    vy: 0,
+    connections: [
+      ...new Set([
           ...(node.connections || []),
           ...(apiData.edges || [])
             .filter(e => e.from === node.id)
@@ -32,25 +34,34 @@ export const transformGraphData = (apiData) => {
   // In src/utils/graphUtils.js
 // In src/utils/graphUtils.js
 export const fetchGraphData = async () => {
-    return {
-      nodes: [
-        { id: 1, x: 300, y: 400 },  // Left
-        { id: 2, x: 500, y: 400 },  // Center
-        { id: 3, x: 700, y: 400 },  // Right
-        { id: 4, x: 400, y: 300 },  // Top-left
-        { id: 5, x: 600, y: 300 },  // Top-right
-        { id: 6, x: 400, y: 500 },  // Bottom-left
-        { id: 7, x: 600, y: 500 }   // Bottom-right
-      ],
-      edges: [
-        // Horizontal connections
-        { from: 1, to: 2 }, { from: 2, to: 3 },
-        // Upper triangle
-        { from: 1, to: 4 }, { from: 4, to: 2 }, { from: 2, to: 5 }, { from: 5, to: 3 },
-        // Lower triangle
-        { from: 1, to: 6 }, { from: 6, to: 2 }, { from: 2, to: 7 }, { from: 7, to: 3 },
-        // Cross connections
-        { from: 4, to: 5 }, { from: 6, to: 7 }
-      ]
-    };
+  return {
+    nodes: [
+      { 
+        id: 1, 
+        x: 300, 
+        y: 400,
+        color: '#ff4444',
+        icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png' // Red node with GitHub icon
+      },
+      { 
+        id: 2, 
+        x: 500, 
+        y: 400,
+        color: '#4CAF50',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2111/2111615.png' // Green node with Slack icon
+      },
+      { 
+        id: 3, 
+        x: 700, 
+        y: 400,
+        color: '#2196F3',
+        icon: 'https://cdn-icons-png.flaticon.com/512/733/733585.png' // Blue node with Twitter icon
+      }
+    ],
+    edges: [
+      { from: 1, to: 2 },  // Connect left to center
+      { from: 2, to: 3 },  // Connect center to right
+      { from: 1, to: 3 }   // Cross-connect left to right
+    ]
   };
+};
