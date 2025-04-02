8f84aa7d3e2ab4c8cbcf1befd5677425583f5e84
daniel G <djdaniel1100@gmail.com>
2025-04-01 20:11:09 -0700

    better page margins and fixed background

--- super-dynamic-graphs/src/App.jsx
+++ super-dynamic-graphs/src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StaticSidebar from './components/StaticSidebar/StaticSidebar';
 import HomePage from './components/HomeComponents/Home';
 import LearnPage from './components/GraphComponents/Graph';
+import "./App.css"
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <StaticSidebar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/learn" element={<LearnPage />} />
-          <Route path="/progress" element={<div>Progress Page</div>} />
-          <Route path="/settings" element={<div>Settings Page</div>} />
-        </Routes>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/learn" element={<LearnPage />} />
+            <Route path="/progress" element={<div>Progress Page</div>} />
+            <Route path="/settings" element={<div>Settings Page</div>} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
