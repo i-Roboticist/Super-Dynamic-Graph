2b13a94401036702d27f69e9e8c06f686e3c25d9
TheRealDannyG <djdaniel1100@gmail.com>
2025-04-01 18:41:56 -0700

    added some placeholder pages

---
+++ super-dynamic-graphs/src/components/HomeComponents/Home.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Welcome to Knowledge Tree</h1>
+            <p>Homepage content goes here</p>
+        </div>
+    );
+};
+
+export default HomePage;
--- super-dynamic-graphs/src/components/StaticSidebar/StaticSidebar.jsx
+++ super-dynamic-graphs/src/components/StaticSidebar/StaticSidebar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
 import { FaFire, FaPoo } from 'react-icons/fa';
 import './StaticSidebar.css';
@@ -5,19 +6,21 @@ import './StaticSidebar.css';
 const StaticSidebar = () => {
     return (
         <div className="static-sidebar">
-            <SideBarIcon icon={<FaFire size="28" />} text="Home" />
-            <SideBarIcon icon={<BsPlus size="32" />} text="Learn" />
-            <SideBarIcon icon={<BsFillLightningFill size="20" />} text="Progress" />
-            <SideBarIcon icon={<FaPoo size="20" />} text="Settings" />
+            <SideBarLink icon={<FaFire size="28" />} text="Home" to="/" />
+            <SideBarLink icon={<BsPlus size="32" />} text="Learn" to="/learn" />
+            <SideBarLink icon={<BsFillLightningFill size="20" />} text="Progress" to="/progress" />
+            <SideBarLink icon={<FaPoo size="20" />} text="Settings" to="/settings" />
         </div>
     );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
-    <div className="sidebar-icon">
-        {icon}
-        <span className="sidebar-tooltip">{text}</span>
-    </div>
+const SideBarLink = ({ icon, text = 'tooltip 💡', to }) => (
+    <Link to={to} className="sidebar-link">
+        <div className="sidebar-icon">
+            {icon}
+            <span className="sidebar-tooltip">{text}</span>
+        </div>
+    </Link>
 );
 
 export default StaticSidebar;
